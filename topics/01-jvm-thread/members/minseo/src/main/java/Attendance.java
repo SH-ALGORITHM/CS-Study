c5a0f969139847f9da6c1385889f476597c5276e
김민서 <minseokim0113@gmail.com>
2026-05-08 00:45:45 +0900

    refactor(w01): s3 introduce Attendance interface for multi-solution testing

--- topics/01-jvm-thread/members/minseo/src/main/java/Attendance.java
+++ topics/01-jvm-thread/members/minseo/src/main/java/Attendance.java
@@ -1,15 +1,3 @@
-public class Attendance {
-    // 출근 도장
-    private boolean checkedIn = false;
-
-    // 출근 도장 찍는 로직
-    public boolean checkIn() {
-
-        if (!checkedIn) {
-            checkedIn = true;
-            return true;
-        }
-
-        return false;
-    }
+public interface Attendance {
+    boolean checkIn();
 }
