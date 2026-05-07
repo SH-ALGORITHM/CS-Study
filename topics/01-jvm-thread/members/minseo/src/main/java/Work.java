6b10efc23d723ab9cabadfd5330f6aa956a6102c
김민서 <minseokim0113@gmail.com>
2026-05-08 00:47:03 +0900

    feat(w01): s3 resolve visibility issue with volatile and finalize logs

--- topics/01-jvm-thread/members/minseo/src/main/java/Work.java
+++ topics/01-jvm-thread/members/minseo/src/main/java/Work.java
@@ -1,7 +1,8 @@
 public class Work extends Thread {
 
     // 퇴근 도장
-    boolean checkOut = false;
+    // volatile을 붙이면 캐시 쓰지 말고 메인 메모리에서 직접 보라는 뜻
+    volatile boolean checkOut = false;
 
     public void run() {
         while (!checkOut) {
